0a2a15b2d5d477133f0f298f06f952f6d832fee9
André Goerres <andre.goerres+git@gmail.com>
2014-08-13 11:28:45 +0200

    Include arguments and fancier output.
    • Optional arguments let's the user overwrite the default settings at launch. Settings are for offline_min time and remote server.
    • The output now prints out the disconnect immediately and completes the line if the disconnect is longer than the offline_min setting, otherwise it cleans the line.

--- checkConnection.py
+++ checkConnection.py
@@ -1,46 +1,82 @@
 #! /usr/bin/env python
 """
-Check for an available internet conneciton by trying to get a DNS request
+Check for an available internet conneciton by trying to get a DNS response
 and then connect to it.
 """
 
+import sys
 import socket
 import time
+import argparse
 
+# Default values
 REMOTE_SERVER = "www.google.de"
+OFFLINE_MIN = 1   # (in seconds) disregard short connection losses
 
 
-def is_connected():
+def is_connected(server):
     """Tries to get a DNS reqeust and returns the success as boolean."""
     try:
         """See if we can resolve the host name.
         Tells us if there is a DNS listening."""
-        host = socket.gethostbyname(REMOTE_SERVER)
+        host = socket.gethostbyname(server)
 
         """Connect to the host.
         Tells us if the host is actually reachable."""
         socket.create_connection((host, 80), 2)
         return True
     except IOError:
-        return False
+        pass
     return False
 
 
-def check_connection():
+def check_connection(args):
     """Continuously check the status of the connection."""
-    connectionLost = -1
+    connectionLostAt = -1
     while True:
         # print "Testing at {0}".format(time.strftime("%c"))
-        if not is_connected():
-            if connectionLost < 0:
-                connectionLost = time.time()
+        if not is_connected(args.remote_server):
+            if connectionLostAt < 0:
+                connectionLostAt = time.time()
+                print "Connection lost at {0}".format(time.strftime("%c")),
+                sys.stdout.flush()
         else:
-            if connectionLost > 0:
-                if time.time() - connectionLost > 1:
-                    print("Connection lost at {0} and back online at {1}  (Offline for {2:.0f} s)".format(time.strftime("%c", time.localtime(connectionLost)), time.strftime("%c"), (time.time() - connectionLost)))
-                connectionLost = -1
-            time.sleep(1)
+            if connectionLostAt > 0:
+                # print out something if disconnect is longer than offline_min
+                if time.time() - connectionLostAt > args.offline_min:
+                    print "and back online at {0}  (Offline for {1:.0f} s)".format(
+                        time.strftime("%X"),
+                        (time.time() - connectionLostAt)
+                    )
+                # otherwise reset current output line
+                else:
+                    print "\r"+50*" "+"\r",
+                    sys.stdout.flush()
+                connectionLostAt = -1
+
+        # wait a bit
+        time.sleep(0.5)
+
+
+def main():
+    """The main function that is called automatically."""
+    parser = argparse.ArgumentParser(
+        description='Check for an available internet connection by trying \
+            to get a DNS response and then connect to it.')
+    parser.add_argument(
+        '-o', '--offline-min',
+        type=int, default=OFFLINE_MIN,
+        help="disconnects shorter than this (in seconds) are disregarded")
+    parser.add_argument(
+        '--remote-server',
+        type=str, default=REMOTE_SERVER,
+        help="the remote server to connect to")
+    args = parser.parse_args()
+
+    # now check the connection
+    check_connection(args)
+
 
 
 if __name__ == "__main__":
-    check_connection()
+    main()
